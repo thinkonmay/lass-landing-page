13799b185ac6e855db2c6f484a1a92d47137b589
carosaome <sieunhankiet@gmail.com>
2024-02-07 23:12:25 +0700

    header reponsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,15 @@
+"use client"
+import { useState } from 'react';
 import Icon from '../icon';
 import './header.scss'
 import Image from 'next/image'
 import Link from 'next/link'
- 
+
 
 
 function Header() {
+	const [openNav, setOpenNav] = useState(false)
+
 	return (
 		<div className='header '>
 			<div className='grid wide wrapperHeader'>
@@ -18,14 +22,32 @@ function Header() {
 					</div>
 				</div>
 
-				<ul className='right'>
+				<ul className='rightDesktop'>
 					<li><Link className='link' href="/services">Dịch vụ</Link></li>
 					<li><Link className='link' href="/#">Về Thinkmay</Link></li>
 					<li><Link className='link' href="/contact">Liên hệ</Link></li>
-					<Link href='https://dev-thinkmay.netlify.app' className='btn btn-link' >CloudPC portal<Icon className={"mb-[1px]"} src='game'/> </Link>
+					<Link href='https://dev-thinkmay.netlify.app' className='btn btn-link' >CloudPC portal<Icon className={"mb-[1px]"} src='game' /> </Link>
 				</ul>
-			
-		</div>
+
+				<div className='rightMobile'>
+					<button onClick={() => setOpenNav(true)}>
+						<Icon src='3-line'></Icon>
+					</button>
+
+					  <div className={openNav ? "content show" : "content"}>
+						<div className='head'>
+							<Link className='link bold' href="/">Trang chủ</Link>
+							<button onClick={() => setOpenNav(false)}><Icon src='x'></Icon></button>
+						</div>
+
+						<ul>
+							<li><Link className='link' href="/services">Dịch vụ</Link></li>
+							<li><Link className='link' href="/#">Về Thinkmay</Link></li>
+							<li><Link className='link' href="/contact">Liên hệ</Link></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div >);
 }
 
