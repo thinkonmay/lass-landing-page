a3a28876acbd6937c4a2791a8d09168eed0b4e3e
carosaome <sieunhankiet@gmail.com>
2024-02-16 08:36:52 +0700

    add text typed

--- src/components/herosection/textTransform.tsx
+++ src/components/herosection/textTransform.tsx
@@ -1,8 +1,27 @@
+"use client"
+import { TypeAnimation } from "react-type-animation";
+
+
 const TextTransform = () => {
     return (
-        <span className="textTransform">
-            làm đồ họa, dựng phim, thiết kế kiến trúc và chơi game
-        </span>
+        <TypeAnimation
+            preRenderFirstString={true}
+            sequence={[
+                500,
+                'làm đồ họa', // initially rendered starting point
+                1000,
+                'dựng phim',
+                1000,
+                'thiết kế kiến trúc',
+                1000,
+                'và chơi game',
+                500,
+            ]}
+            speed={50}
+            //style={{ fontSize: '2em' }}
+            repeat={Infinity}
+            className="textTransform"
+        />
     );
 };
 
