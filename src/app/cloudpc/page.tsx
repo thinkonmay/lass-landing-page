beb8b4fb358fd6ae35c763f705cbe79db3673cb7
carosaome <sieunhankiet@gmail.com>
2024-03-02 21:49:47 +0700

    Reason

--- src/app/cloudpc/page.tsx
+++ src/app/cloudpc/page.tsx
@@ -51,42 +51,64 @@ const HeroSection = () => {
     )
 }
 
+const listReason = [
+    {
+        title: 'Mọi thiết bị, mọi nơi',
+        text: 'Biến bất kì chiếc laptop, Điện thoai, thậm chí cả tivi thành 1 chiếc PC mạnh mẽ. ',
+        icon: '',
+    },
+    {
+        title: 'Chỉ từ 299k/tháng',
+        text: 'Gia hạn mỗi tháng. Hủy ngay lập tức khi bạn muốn.',
+        icon: '',
+    },
+    {
+        title: 'Phầm mềm bản quyền',
+        text: 'Đều đã được cài sẵn & có bản quyền. Không mất thời gian download & crack.',
+        icon: '',
+    },
+    {
+        title: 'Bảo mật tuyệt đối',
+        text: 'Mọi dữ liệu đều được lưu trên cloud, không còn nỗi lo mất data.',
+        icon: '',
+    },
 
+]
 const Reason = () => {
 
-    const Card = () => {
 
-        return (
+    const renderCard = () => {
+
+        return listReason.map(rea => (
             <div className='l-3 col '>
                 <div className="card">
-                    <Icon src='laptop' />
-                    <div className="title">
-                        Mọi thiết bị, mọi nơi
+                    <div className="">
+                        <Icon src='laptop' />
+                        <h5 className="title">
+                            {rea.title}
+                        </h5>
+                        <p className="text">
+                            {rea.text}
+                        </p>
                     </div>
-                    <p className="text">
-                        Biến bất kì chiếc laptop, Điện thoai, thậm chí cả tivi thành 1 chiếc PC mạnh mẽ.
-                    </p>
 
-                    <button>Chi tiết thiết bị</button>
+                    <a className='btn-link'>Chi tiết thiết bị <Icon src='arrow-right'></Icon></a>
 
                 </div>
             </div>
-        )
-    }
 
+        ))
+    }
     return (
         <div className="reason">
             <div className="grid wide mainContent">
-                <div className="wrapperTitle">
+                <div className="wrapperTitle text-center">
                     <h2 className='title'>4 lí do bạn nên có 1 chiếc <span className="highlight"> CLOUD PC</span></h2>
                     <p className="subTitle"></p>
                 </div>
 
                 <div className="cards row">
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
+                    {renderCard()}
                 </div>
             </div>
         </div>
