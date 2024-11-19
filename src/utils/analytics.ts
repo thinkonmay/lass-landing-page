0f09d730e5e12cd42089bcd40db9264bbe207317
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-11-19 04:25:51 +0000

    change domain

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const APP_REDIRECT = 'https://play.thinkmay.net';
-export const APP_DOMAIN = 'https://play.0.thinkmay.net';
+export const APP_DOMAIN = 'https://play.thinkmay.net';
 const APP_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzIzMTM2NDAwLAogICJleHAiOiAxODgwOTAyODAwCn0.SdW2AcXzhRFNBt9HmJw6sKa7lWDmVjbXdRF1mIjrDao';
 export const supabaseLocal = createClient(APP_DOMAIN, APP_KEY);
